15dc9b665f803983e1fa3acc9627ba6c51e79de6
rbtechme <rbtechme@gmail.com>
2025-02-06 13:56:25 +0530

    add get, post, delete, and patch method

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
+/* app.use("/", (req, res, next) => {
    if (req.path === "/") {
       return res.send("This is dashboard");
    }
    next(); // Pass control to the next middleware
+}); */
+
+app.get("/user",(req, res) =>{
+   res.send({name:"raju", surname:"Bhalerao"});
 });
+
+app.post("/user", (req, res)=>{
+    res.send("Data save in the database!");
+});
+
+app.patch("/user", (req, res)=>{
+    res.send("Data udate in the database!");
+});
+
+app.delete("/user", (req, res)=>{
+    res.send("Sorry, your user data is lost!");
+})
 
 app.use("/test", (req, res) => {
     res.send("This is test");
