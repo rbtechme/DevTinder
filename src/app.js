6f8e8eb45ecd85015fa2dff80e709b648495ef65
rbtechme <rbtechme@gmail.com>
2025-02-07 14:43:39 +0530

    multiple rounter

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.delete("/user", (req, res)=>{
 app.use("/test", (req, res) => {
     res.send("This is test");
 });
-*/
+
 
 app.get("/*fly/", (req, res) => {
     res.send({name:"raju", surname:"Bhalerao"});
@@ -38,6 +38,38 @@ app.get("/user/:userId/:username/:rollno", (req, res) => {
     console.log(req.params);
     res.send({ name: "raju", surname: "Bhalerao" });
 });
+*/
+
+app.use(
+    "/user", [(req, res, next) => {
+        console.log("Handling the router");
+        // res.send("Send response !");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling rounter 2");
+        //res.send("Send Response 2");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling rounter 3");
+        //res.send("Send Response 3");
+        next();
+    }],
+    (req, res, next) => {
+        console.log("Handling rounter 4");
+        //res.send("Send Response 4");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling rounter 5");
+        res.send("Send Response 5");
+        next();
+    }
+
+);
+
+
 
 app.listen(7777, () => {
     console.log("Server started on port: http://localhost:7777");
