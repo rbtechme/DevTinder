27bae480812ae2abd966a420e4f7700e1e309bc3
rbtechme <rbtechme@gmail.com>
2025-02-12 11:57:37 +0530

    validation for allow filds

--- src/app.js
+++ src/app.js
@@ -11,12 +11,16 @@ app.post("/signup", async (req, res) =>{
 
     try {
 
+     if(req.body.skills.length > 10){
+        throw new Error("Skills not alllow more than 10");
+     }   
     const user = new User(req.body);
     await user.save();
+
     res.send("data save successfully in the DB");
         
     } catch (error) {
-        res.status(404).send("User not save", error.message);
+        res.status(404).send("Something went wrong!, " + error.message);
     }
 });
 
@@ -69,15 +73,37 @@ app.delete("/user", async (req, res) =>{
 });
 
 //update user data
-app.patch("/user", async (req, res) => {
+app.patch("/user/:id", async (req, res) => {
+    const userID = req.params?.id;
+    const data = req.body;
    try {
-       const user = await User.findByIdAndUpdate(req.body.UserId, req.body);
+      
+       if(data.skills.length > 10){
+        throw new Error("Skills not allow more than 10");
+       }
+
+       const ALLOW_UPDATE = ["firstName","lastName","gender", "age", "skills","about","photoUrl"];
+
+       const isAllowUpdate = Object.keys(data).every((k)=>{
+            ALLOW_UPDATE.includes(k);
+       });
+
+       if(!isAllowUpdate){
+         throw new Error("update not allow");
+       }
+
+
+       const user = await User.findByIdAndUpdate({_id:userID}, data, {
+        returnDocument: "after",
+        runValidators:true
+       });
+       
        if(!user){
         res.status(404).send("User id not found or something went wront!");
        }
        res.status(200).send({ message: "Data updated successfully", user });
    } catch (error) {
-      res.status(500).send("something went wrong")
+      res.status(500).send("something went wrong" + error.message)
    }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,60 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName: { type : String},
-    lastName: { type : String},
-    gender: { type : String},
-    emailId: { type : String},
-    password: { type : String},
-    age: { type : Number}
+    firstName: 
+    { 
+        type : String,
+        minLength:3,
+        required: true
+
+    },
+    lastName: 
+    { type : String
+
+    },
+    gender: 
+    { 
+        type : String,
+         validate(value){
+            if(!["Male","Female", "Other"].includes(value)){
+                throw new Error("Gender data not valid");
+            }
+         },
+         
+    },
+    emailId: 
+    { 
+        type : String,
+        required:true,
+        lowercase:true,
+        trim:true,
+        unique:true
+
+    },
+    password: 
+    { 
+        type : String
+
+    },
+    age: 
+    { 
+        type : Number,
+        min:18
+    },
+    about:{
+        type: String,
+        default:"This is default text insertion example"
+    },
+    photoUrl:{
+        type: String,
+        default:"https://picsum.photos/id/237/200/300"
+    },
+    skills:
+    {
+        type:[String]
+    }
+},{
+    timestamps:true
 });
 
 module.exports = mongoose.model("User", userSchema);
