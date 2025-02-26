27702a7fff5bfdf64c8f6ccec694c71ab46e3a09
rbtechme <techcarecode@gmail.com>
2025-02-26 08:58:32 +0530

    add cors Lib

--- src/app.js
+++ src/app.js
@@ -1,24 +1,33 @@
 const express = require("express");
-const {connectDB} = require("./config/database");
-const cookieParser = require('cookie-parser');
+const { connectDB } = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/', require('./router/auth'));
-app.use('/', require('./router/profile'));
-app.use('/', require('./router/request'));
-app.use('/', require('./router/forgotPassword'));
-app.use('/', require('./router/user'));
+app.use("/", require("./router/auth"));
+app.use("/", require("./router/profile"));
+app.use("/", require("./router/request"));
+app.use("/", require("./router/forgotPassword"));
+app.use("/", require("./router/user"));
 
 connectDB()
-.then(()=> {
+  .then(() => {
     console.log("database connection connected");
     app.listen(7777, () => {
-        console.log("Server started on port: http://localhost:7777");
+      console.log("Server started on port: http://localhost:7777");
     });
-})
-.catch(() => {
+  })
+  .catch(() => {
     console.error("connect can not connected");
-});
+  });
--- src/router/auth.js
+++ src/router/auth.js
@@ -1,58 +1,65 @@
-const express = require('express');
+const express = require("express");
 const User = require("../models/user");
-const {validateSignUpData, validateSignInData} = require("../utils/validation");
-const bcrypt = require('bcrypt');
+const {
+  validateSignUpData,
+  validateSignInData,
+} = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
-const authRouter = express.Router();    
+const authRouter = express.Router();
 
-authRouter.post("/signup", async (req, res) =>{
+authRouter.post("/signup", async (req, res) => {
+  try {
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
 
-    try {
-        validateSignUpData(req);
-        const {firstName, lastName, emailId, password} = req.body;
-        const passwordHash = await bcrypt.hash(password, 10);
- 
-        const user = new User({
-            firstName, lastName, emailId, password:passwordHash
-        });
-        await user.save();
-        res.send({status:true, message:"data save successfully in the DB", user});
-        
-    } catch (error) {
-        const errMsg = error.message;
-        res.status(500).send({status:false, errMsg});
-    }
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+    await user.save();
+    res.send({
+      status: true,
+      message: "data save successfully in the DB",
+      user,
+    });
+  } catch (error) {
+    const errMsg = error.message;
+    res.status(500).send({ status: false, errMsg });
+  }
 });
 
-authRouter.post("/login", async (req, res)=>{
-    try {
-        validateSignInData(req);
-        const {emailId, password} = req.body;
-        const user = await User.findOne({emailId: emailId});
-        if(!user){
-            throw new Error("Invalid Credential");
-        }
-        const isPassword = await user.validatePassword(password);
-        if(!isPassword){
-            throw new Error("Invalid Credential");
-        }else{
-            const token = await user.getJWT();
-            res.cookie('token',token, {
-                expires: new Date(Date.now() + 3600000), //1 hour
-            });
-            res.status(200).send({status:true, message:"login Successful!"});
-        }
-   
-    } catch (error) {
-        const errMsg = error.message;
-        res.status(500).send({status:false, errMsg});
+authRouter.post("/login", async (req, res) => {
+  try {
+    validateSignInData(req);
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credential");
     }
+    const isPassword = await user.validatePassword(password);
+    if (!isPassword) {
+      throw new Error("Invalid Credential");
+    } else {
+      const token = await user.getJWT();
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 3600000), //1 hour
+      });
+      res.status(200).send({ status: true, data: user });
+    }
+  } catch (error) {
+    const errMsg = error.message;
+    res.status(500).send({ status: false, errMsg });
+  }
 });
 
-authRouter.post("/logout", async (req, res)=>{
-    res.cookie('token', null, {
-        expires: new Date(Date.now())
-    })
-    res.send({status:true, message:"logout successfully!"});
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send({ status: true, message: "logout successfully!" });
 });
 module.exports = authRouter;
